d100b39c2d14e2465d72f981da2c5fb03b507d89
spencers1 <spencer.sean22@gmail.com>
2017-11-15 12:25:47 -0500

    Update Driver.java

--- Lab4AVLTree/src/Driver.java
+++ Lab4AVLTree/src/Driver.java
@@ -4,21 +4,25 @@ public class Driver {
         AVLTree tree = new AVLTree();
 //        tree.makeTree(12);
         tree.insert(tree.getRoot(), 33);
-
+        tree.printTree();
         tree.insert(tree.getRoot(), 23);
-
-        tree.insert(tree.getRoot(), 16);
-
+        tree.printTree();
         tree.insert(tree.getRoot(), 22);
-
+        tree.printTree();
         tree.insert(tree.getRoot(), 25);
-
+        tree.printTree();
         tree.insert(tree.getRoot(), 28);
+        tree.printTree();
         tree.insert(tree.getRoot(), 80);
+        tree.printTree();
         tree.insert(tree.getRoot(), 51);
+        tree.printTree();
         tree.insert(tree.getRoot(), 70);
+        tree.printTree();
         tree.insert(tree.getRoot(), 85);
+        tree.printTree();
         tree.insert(tree.getRoot(), 92);
+        tree.printTree();
 
 
 
